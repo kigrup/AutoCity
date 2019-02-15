44a620fa66b422b0d1bd2f1bc07819d2eda88330
Salt4f <47352634+Salt4f@users.noreply.github.com>
2019-02-15 20:59:25 +0100

    Update

--- source/main.cpp
+++ source/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include "LTexture.h"
+#include "LTimer.h"
 #include "game.h"
 
 int main(int argc, const char * argv[]) {
@@ -7,4 +9,55 @@ int main(int argc, const char * argv[]) {
         return 0;
     }
     
+    SDL_Event e;
+    
+    LTimer fpsTimer;
+    
+    bool quit = false;
+    
+    while (!quit) {
+        
+        fpsTimer.start();
+        
+        while (SDL_PollEvent( &e ) != 0) {
+            
+            //std::cout << "Event loop starts!" << std::endl;
+            
+            if( e.type == SDL_QUIT ) quit = true;
+            
+            if (e.type == SDL_KEYDOWN) {}
+        
+            SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+            SDL_RenderClear(gRenderer);
+        
+            
+            SDL_RenderPresent(gRenderer);
+            
+            int frameTicks = fpsTimer.getTicks();
+            if( frameTicks < TICK_PER_FRAME )
+            {
+                //Wait remaining time
+                SDL_Delay( TICK_PER_FRAME - frameTicks );
+            }
+        }
+        
+        SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);
+        SDL_RenderClear(gRenderer);
+    
+    
+    
+        SDL_RenderPresent(gRenderer);
+        
+        int frameTicks = fpsTimer.getTicks();
+        if( frameTicks < TICK_PER_FRAME )
+        {
+            //Wait remaining time
+            SDL_Delay( TICK_PER_FRAME - frameTicks );
+        }
+    }
+    
+    //image.free();
+    IMG_Quit();
+    SDL_Quit();
+    return 0;
 }
