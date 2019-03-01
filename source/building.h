410cbc28ac16a2744c293a477f1c8025495808c5
Salt4f <47352634+Salt4f@users.noreply.github.com>
2019-03-01 20:44:37 +0100

    Update

--- source/building.h
+++ source/building.h
@@ -2,5 +2,25 @@
 #define building_h
 
 #include <iostream>
+#include <string>
+#include <vector>
+#include "LTexture.h"
+#include "villager.h"
+
+class Building {
+private:
+    int id;
+    std::vector<std::string> recovery;
+    
+    LTexture sprite;
+    
+    Villager * b_Villager;
+    
+public:
+    Building(int id, std::vector<std::string>& recovery, std::string path);
+    
+    void villagerIn(Villager * bVillager);
+};
+
 
 #endif /* building_h */
