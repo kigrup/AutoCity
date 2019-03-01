caa6946d87a19b272bf62a4e18f8969891abfe20
Dewiken <48120219+Dewiken@users.noreply.github.com>
2019-03-01 21:05:51 +0100

    Update building.cpp

--- source/building.cpp
+++ source/building.cpp
@@ -1,1 +1,14 @@
 #include "building.h"
+
+Building::Building(int id; std::vector<std::string>& recovery, int max, std::string path, SDL_Renderer * gRenderer) 
+{
+	this->id = id;
+	this->recovery = recovery;
+	MAX_PEOPLE = max;
+	bRenderer = gRenderer;
+
+
+	sprite.loadFromFile(path,gRenderer);
+
+
+}
