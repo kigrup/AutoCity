ce0c62b04987df3a0f92855df4d8b7756a711e87
vitipro <47673221+vitipro@users.noreply.github.com>
2019-03-01 22:16:04 +0100

    Update building.cpp

--- source/building.cpp
+++ source/building.cpp
@@ -12,7 +12,27 @@ Building::Building(int id, std::vector<std::string>& recovery, int max, std::str
     }
 
 	sprite.loadFromFile(path,gRenderer);
+
 }
+
+void Building::reordenar() 
+{ 
+    int count = 0;  // Count of non-zero elements 
+    int n = vec.size();
+    // Traverse the array. If element encountered is non- 
+    // zero, then replace the element at index 'count'  
+    // with this element 
+    for (int i = 0; i < n; i++) 
+        if (vec[i] != NULL) 
+            vec[count++] = vec[i]; // here count is  
+                                   // incremented 
+  
+    // Now all non-zero elements have been shifted to  
+    // front and  'count' is set as index of first 0.  
+    // Make all elements 0 from count to end. 
+    while (count < n) 
+        vec[count++] = NULL; 
+} 
 
 void Building::villagerIn(Villager * bVillager) {
     if (lastVillager < MAX_PEOPLE - 1) {
@@ -28,7 +48,7 @@ void Building::villagerOut(Villager * bVillager) {
             found = true;
             this->bVillager[i] = NULL;
             --lastVillager;
-            //funcion ordenar
+            reordenar();
             break;
         }
     }
