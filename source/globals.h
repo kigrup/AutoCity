9973931e935e5af7ad83b4c0769ee2ada4d7795d
Salt4f <47352634+Salt4f@users.noreply.github.com>
2019-03-01 21:15:35 +0100

    Update

--- source/globals.h
+++ source/globals.h
@@ -3,14 +3,14 @@
 
 #include <string>
 
-const int SCREEN_WIDTH = 720;
-const int SCREEN_HEIGHT = 560;
+const int SCREEN_WIDTH = 736; //23 tiles
+const int SCREEN_HEIGHT = 576; //18 tiles
 const int FPS = 60;
 const int TICK_PER_FRAME = 1000 / FPS;
 
 const std::string TITLE = "AutoCity";
 
-const int TILE_WIDTH = 80;
-const int TILE_HEIGHT = 80;
+const int TILE_WIDTH = 32;
+const int TILE_HEIGHT = 32;
 
 #endif /* globals_h */
